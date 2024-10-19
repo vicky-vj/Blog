d0585394477a7ec533b0f91b9724c550e92b8d42
Vj-vicky <vigneshraja@stoneagesolutions.com>
2024-10-19 14:04:55 +0530

    New Changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'blog';
+
+
+  ngOnInit() {
+    //     let link = document.createElement('a');
+    // link.setAttribute('type', 'hidden');
+    // link.href = 'assets/file';
+    // link.download = path;
+    // document.body.appendChild(link);
+    // link.click();
+    // link.remove();
+  }
+}
